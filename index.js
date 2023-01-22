77d0abcb8a2ecd33fee4f13a7c255ec5bfc63fc1
sasaplus1 <sasaplus1@gmail.com>
2023-01-23 01:40:49 +0900

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+  return a + b;
+}
+
+window.result = sum(1, 4);
--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,8 @@
+import fs from 'node:fs';
+
 import { test, expect } from '@playwright/test';
 
-test('has title', async function({ page }) {
+test('test1', async function({ page }) {
   await page.goto('https://example.com/');
   await page.setContent(`
     <script>window.result = 1;</script>
@@ -11,4 +13,17 @@ test('has title', async function({ page }) {
   });
 
   await expect(result).toBe(1);
+});
+
+test('test2', async function({ page }) {
+  await page.goto('https://example.com/');
+  await page.setContent(`
+    <script>${fs.readFileSync('./index.js', 'utf8')}</script>
+  `);
+
+  const result = await page.evaluate(function() {
+    return window.result;
+  });
+
+  await expect(result).toBe(5);
 });
